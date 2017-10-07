f9b89c84abafb2d24d469136c22ec57ac30b59be
Anne Whitman <anne.whitman@rutgers.edu>
2017-10-07 16:03:39 -0400

    Testing merging branches on quantum change

--- my_pthread_t.h
+++ my_pthread_t.h
@@ -14,7 +14,7 @@
 #define MAX_THREADS 50 //max threads
 #define MAX_MUTEX 50 //max mutexes
 #define STACK_SIZE 4096 //size of stack in bytes
-#define QUANTUM 25 //predefined in project spec
+#define QUANTUM 25000 //predefined in project spec as 25ms - converted to microseconds
 #define CYCLES 5 //how many full maintenance cycles before moving up one level of queue
 
 
