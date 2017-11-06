7a6fc1df9a72187d203d5e2c2027f4c40b052cbd
Michael Mazzola <mjm2z@hotmail.com>
2017-11-06 04:13:29 +0000

    myallocate.h

--- myallocate.h
+++ myallocate.h
@@ -7,7 +7,26 @@
 #define free(x) mydeallocate(x,__FILE__,__LINE__,THREADREQ)
 #define MEMORYSIZE 8388608
 #define PAGESIZE sysconf( _SC_PAGE_SIZE)
+#define NUMOFPAGES (MEMORYSIZE / PAGESIZE)
 #endif
+
+
+
+typedef enum _bool{
+	FALSE, TRUE
+} bool;
+
+typedef struct _memStruct{
+	//int sizeLeft;	//initally set = to page size	
+	int currUsed;
+	int pageCount;
+	bool inUse;
+	struct _memStruct* next;
+	struct _memStruct* prev;
+}memStruct;
+
+
+
 
 char * myallocate(int size, char * file, int line, int type);
 void mydeallocate(char* ptr, char* file, int line, int type);
