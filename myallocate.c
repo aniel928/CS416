e3915e2f816a32ca45a8e9df90c567f0b634679a
Michael Mazzola <mjm2z@hotmail.com>
2017-11-08 19:26:53 +0000

    myallocate.c edited - This is working but it's missing a lot (i.e. page table, memprotect, etc..)

--- myallocate.c
+++ myallocate.c
@@ -18,7 +18,7 @@
 //check to see if there is no room (in else)
 //free the pointer that was given to free
 //have to fix segfault in if else main, won't allow for mallocing more than there is
-
+//when we implement swapping we shouldnt do it if the space left is smaller than the metadata size
 static char memory[MEMORYSIZE];	//create huge memory buffer
 memStruct * memHead = NULL;
 memStruct * memNew = NULL;
@@ -26,29 +26,37 @@ memStruct * memFollow = NULL;
 memStruct * memNext = NULL;
 int pagesUsed = 0;
 
+//This function is purely for testing, leaving in now but not necessary when we submit
 showData(){
 	memStruct * memTest = memHead;
 	printf("----------PRINTING LINKED LIST DATA----------\n");
 	int i = 0;
+	int pc = 0;
 	char* boolVal = "FALSE";
 	if (memTest == NULL){
-		printf("what?\n");
+		printf("All empty?\n");
 	}
 	else{
 		while (memTest!=NULL){
+	
 			if(memTest->inUse == TRUE){
 				boolVal = "TRUE";
 			}
 			else{
 				boolVal = "FALSE";
 			}
-			printf("Page number: %d, Page count: %d, inUse? %s, Addr: %d\n", i, memTest->pageCount, boolVal, memTest);
+			printf("Page number: %d, Page count: %d, inUse? %s, index: %d\n", i, memTest->pageCount, boolVal, pc*PAGESIZE);
+			pc += memTest->pageCount;
 			memTest = memTest->next;
 			i++;
 		}
 	}
+	printf("Size of memory: %d | Number of pages: %d | Page size: %d\n", MEMORYSIZE, NUMOFPAGES, PAGESIZE);
+	printf("Bytes used: %d \t| Pages used: %d\t\n", pc * PAGESIZE, pc);
+	
 }
 
+//Checks to see how many pages have been used, need to switch to page table
 int usedPages(){
 	memStruct * memUP = memHead;
 	int count = 0;
@@ -61,75 +69,68 @@ int usedPages(){
 
 
 char* myallocate(int size, char* file, int line, int type){
-		printf("in myallocate\n");	
-		//I think this is going to cause an issue because I wanted to make it global but it wasn't letting memHead
-		//I tested something and it actually seems to be working fine but I'm leaving this here for now just in case [Don't even know if this array is necessary]
-		char* pageArr[(NUMOFPAGES)]; 
+	char* pageArr[(NUMOFPAGES)]; 
+	int usePgs= usedPages();
 		
-		int usePgs= usedPages();
-		printf("Total amount of pages: %d, pages used so far: %d\n", NUMOFPAGES, usePgs);
-		
-
-		
-		//determining how many pages are needed to return
-		int pageCount = ((size + sizeof(memStruct))/PAGESIZE) + 1;
-		printf("Need %d pages for this Malloc\n", pageCount);
-				
-		//Check if head has been built
-		if (memHead == NULL){	//memHead not built yet
-			printf("Head is NULL\n");
-			int a = 0;
-			int index = 0;
+	//determining how many pages are needed to return
+	int pageCount = ((size + sizeof(memStruct))/PAGESIZE) + 1;
+	printf("Need %d pages for this Malloc\n", pageCount);
 			
-			//creating indexes for each page and storing them in an array
-			while (a <= NUMOFPAGES){//index <=MEMORYSIZE - (PAGESIZE/2)){
-				pageArr[a] = &memory[index];
-				index += PAGESIZE;
-				a++;
-			}
+	//Check if head has been built
+	if (memHead == NULL){	//memHead not built yet
+		printf("Head is NULL\n");
+		int a = 0;
+		int index = 0;
 		
-			//create head
-			memHead = (memStruct*)memory;
-			memHead->pageCount = pageCount;
-			memHead->currUsed = size + sizeof(memStruct);
-			memHead->inUse = TRUE;
-			memHead->prev = NULL;
-			memHead->next = NULL;
-			
-			//returning the front of the string
-			int retAmt = PAGESIZE * pageCount - sizeof(memStruct); //not sure if I have to set the return string to this length, and if so it isn't working
-			printf("returning %d bytes\n", retAmt);			
-			char * retStr = memory + sizeof(memStruct)-1;
-			return retStr;
+		//creating indexes for each page and storing them in an array
+		while (a <= NUMOFPAGES){
+			pageArr[a] = &memory[index];
+			index += PAGESIZE;
+			a++;
 		}
-		else{//head has been previously set
-			printf("Head set, need %d pages\n", pageCount);
-			memNew = memHead;
-			memFollow = memNew;
-			memNew = memNew->next;
-			
-			//find out how many pages have been used
-			int totalPages = memHead->pageCount;	
-			while (memNew!=NULL){
-					totalPages += memNew->pageCount;
-					memNew = memNew->next;
-			}
-			//find next available page
-			memNew = memFollow->next;
+	
+		//create head
+		memHead = (memStruct*)memory;
+		memHead->pageCount = pageCount;
+		memHead->currUsed = size + sizeof(memStruct);
+		memHead->inUse = TRUE;
+		memHead->prev = NULL;
+		memHead->next = NULL;
 		
+		//returning the front of the string
+		int retAmt = PAGESIZE * pageCount - sizeof(memStruct); //not sure if I have to set the return string to this length, and if so it isn't working
+		printf("returning %d bytes\n", retAmt);			
+		void * retStr = (void*)(memory + sizeof(memStruct)-1);
+		return retStr;
+	}
+	else{//head has been previously set
+		printf("Head set, need %d pages\n", pageCount);
+		memNew = memHead;
+		memFollow = memNew;
+		memNew = memNew->next;
+		
+		//find out how many pages have been used
+		int totalPages = usedPages();
+		
+		//reset memNew
+		memNew = memHead->next;
+		int currPages = memHead->pageCount;
+		int k = 0;
 		while (memNew != NULL){
-			if (memNew->inUse == FALSE && memNew->pageCount >= pageCount){
+			if (memNew->inUse == FALSE && memNew->pageCount >= pageCount){ //may need to implement how many bytes are actually left
 				break;
 			}
+			currPages += memNew->pageCount;
 			memFollow = memNew;
-			memNew = memNew->next;		
+			memNew = memNew->next;	
+			k++;
 		}
 		if(memNew == NULL){ //found page is NULL
 			//need to make sure that the available space can hold the requested size
-				if ((usePgs + pageCount) > NUMOFPAGES){
-					printf("Can't give this many bytes/pages for this malloc, returning\n");
-					return ;
-				}			
+			if ((usePgs + pageCount) > NUMOFPAGES){
+				printf("Can't give this many bytes/pages for this malloc, returning\n");
+				return ;
+			}				
 			memNew = (memStruct*)(memory +(totalPages * PAGESIZE - 1)); //This line is the cause of about an hour worth of segfaulting
 			memFollow->next = memNew;
 			memNew->inUse = TRUE;
@@ -139,40 +140,25 @@ char* myallocate(int size, char* file, int line, int type){
 			memNew->next = NULL;
 		}
 		else{//found page that was previously used BUT it was free'd
+			printf("found page that was previously used but it was freed %d\n", memNew->pageCount);
 			memNew->inUse = TRUE;		//set back to in use
 			memNew->currUsed = sizeof(memStruct)+size;
 			//If the amount of pages that this previously used page is more than what is needed, create a new memStruct for the leftover pages and set its inUse to false
 			if (memNew->pageCount > pageCount){
-				//cant get memNext set to the right value
-				int index = (int)memNew;
-				printf("index: %d\n", index);
-				index = index + pageCount * PAGESIZE;
-				index = memory - index;
-				printf("index: %d\n");
-				printf("this should be the index for the next.. %d\n", index);
-				//(memNew->pageCount - pageCount) * PAGESIZE;
-				printf("Creating a new node for extra pages, value of index: %d\n", index);
-				memNext = (memStruct*)(memory + index);
-				printf("current index: %d, the next is %d\n", memNew, memNext);
+				//if the freed section has more pages than the pages needed, proceed accordingly
+				//determine how many more pages there are
+				int index = currPages * PAGESIZE - 1 + pageCount * PAGESIZE;
+				memNext = (memStruct*)(memory +index);
 				memNext->inUse = FALSE;
-				printf("1\n");
 				memNext->pageCount = memNew->pageCount - pageCount;
-				printf("2\n");
 				memNext->next = memNew->next;
-				printf("3\n");
+				memNext->currUsed = sizeof(memStruct);
 				memNext->prev = memNew;
-				printf("4\n");
 				memNew->next = memNext;
-				printf("5\n");
-				memNew->currUsed = sizeof(memStruct);
-				printf("finished\n");
+				memNew->pageCount = pageCount;
 			}
 		}
-		//printf("Need this number: %d, Have these: %d | %d | %d\n", (memory +(totalPages * PAGESIZE - 1)), &memNew, memNew, *memNew);
-		char* retStr = (char*)(memNew +sizeof(memStruct));
-		
-		//+ sizeof(memStruct));//(memory + sizeof(memStruct)+(totalPages * PAGESIZE - 1));
-		//printf("This is the address being returned: %d size: %d\n For some reason its adding 1024 and not 32 like it should be\n", retStr, sizeof(memStruct));
+		void* retStr = (void*)(memNew +sizeof(memStruct));
 		return retStr;
 	}
 }
@@ -184,20 +170,13 @@ void mydeallocate(char* ptr, char* file, int line, int type){
 	int pageNum =  ((ptr +1  - memory)/PAGESIZE + 1)-1;
 	int search = 0;
 	memStruct * memFree = memHead;
-	printf("&ptr = %d, &memory = %d,result is this: %d\n", ptr+1, memory, pageNum);
-	printf("mfpc: %d",memFree->pageCount);
 	while (search <= pageNum && memFree != NULL){
-		printf("search: %d pageNum: %d\n", search, pageNum);
 		search += memFree->pageCount;
-		printf("value of search: %d, (just added %d)\n", search, memFree->pageCount);
 		if (search == pageNum){
-			printf("address being freed: %d\n", memFree);
 			memFree->inUse = FALSE;
 			found = TRUE;
-			printf("By George, I think I've got it.\n");
 			break;
-		}
-		
+		}		
 		memFree = memFree->next;
 	}
 	if (found == FALSE){
@@ -208,20 +187,7 @@ void mydeallocate(char* ptr, char* file, int line, int type){
 	}
 }
 int main(int argc, char** argv){
-	/*rintf("Can i have 4090 bytes?\n");
-	char* test1 = myallocate(4090, __FILE__, __LINE__, 1);
-	
-	char* test2 = myallocate(3,  __FILE__, __LINE__, 1);
-	char* test3 = myallocate(5000, __FILE__, __LINE__, 1);
-	mydeallocate(test3, __FILE__, __LINE__, 1);
-	char* test4 = myallocate(100, __FILE__, __LINE__, 1);
-	
-	char* test5 = myallocate(300,  __FILE__, __LINE__, 1);
-	char* test6 = myallocate(5000, __FILE__, __LINE__, 1);
-	*/
-//	char** tester;
 	int i =0;
-	//while (i<100){
 		printf("memory size: %d\n", MEMORYSIZE);
 	  char	* tester1 = myallocate(1024*1024, __FILE__, __LINE__, 1);
 		char	* tester2 = myallocate(1024*1024, __FILE__, __LINE__, 1);
@@ -232,130 +198,18 @@ int main(int argc, char** argv){
 		char	* tester7 = myallocate(1024*1024, __FILE__, __LINE__, 1);
 		mydeallocate(tester4,__FILE__, __LINE__, 1);
 		char	* tester8 = myallocate(300*300, __FILE__, __LINE__, 1);
-	//	char * tester9 = myallocate (200*500, __FILE__, __LINE__, 1);
-	/*	char	* tester9 = myallocate(1024*1024, __FILE__, __LINE__, 1);
+		char * tester81 = myallocate (200*500, __FILE__, __LINE__, 1);
+		char	* tester9 = myallocate(1024*1024, __FILE__, __LINE__, 1);
 		char	* tester10 = myallocate(1024*1024, __FILE__, __LINE__, 1);
-		char	* tester11 = myallocate(1024*1024, __FILE__, __LINE__, 1);
+		char	* tester11 = myallocate(50*1024, __FILE__, __LINE__, 1);
 		char	* tester12 = myallocate(1024*1024, __FILE__, __LINE__, 1);
+		mydeallocate(tester3,__FILE__,__LINE__, 1);
+		char	* tester13 = myallocate(1200*1024, __FILE__, __LINE__, 1);
+		char	* tester14 = myallocate(350*1024, __FILE__, __LINE__, 1);
 							 
 		printf("BACK\n");
 		i++;
-//	}
-//	malloc(5);*/
-
-showData();
-
-printf("thanks!\n");
-	
-	
-	
-	
-	
+	showData();
+	printf("thanks!\n");
 	
 }
-
-
-
-
-
-/**Dumb Code**/
-
-
-			/*Was used for finding the next free node but don't think I need it
-				memStruct * findNext(){
-				memStruct * memSearch = memHead;
-				if (memHead->next == NULL){
-				printf("next is NULL, create another\n");
-			}
-			else{
-				printf("next is not NULL, search for first NULL\n");
-				while(memSearch != NULL){
-				printf("in while for findNext\n");
-				if (memSearch->inUse == FALSE){
-					break;
-				}
-				printf("before this line\n");
-				memSearch = memSearch->next;	
-				printf("after this line\n");
-			}
-		}
-	printf("out of while\n");
-	return memSearch;
-}*/
-
-	
-			/* This was for adding a new struct after head and setting the inUse to false, but this isn't necessary
-			memNew = (memStruct*)memory+(pageCount * PAGESIZE - 1);
-			//memNew->sizeLeft = 0;
-			memNew->pageCount = 0;
-			memNew->currUsed = sizeof(memStruct);
-			memNew->inUse == FALSE;
-			memNew->prev = memHead;
-			memNew->next = NULL;
-			*/
-
-		//this is for determining the pages needed if EVERY page were to have metadata,
-		//took it out because it only puts metadata in front of the first page
-		/*while (pageCheck + sizeof(memStruct) >= PAGESIZE){
-			pageCheck = pageCheck + sizeof(memStruct) - PAGESIZE;	
-			pageCount++;			
-			}*/
-	
-			/*false
-			printf("head HAS been set\n");
-			memStruct * new;
-			new->sizeLeft = PAGESIZE * pageCount - sizeof(memStruct);
-			new->inUse = TRUE;
-			memStruct * temp = memHead;
-			int i = 1;
-			while (temp->next!= NULL){
-				temp = temp->next;
-				printf("moved to next\n");
-				i++;
-			} 
-			printf("lasdkfjalsd\n");
-			new->next = NULL;
-			new->prev = temp;
-			temp->next = new;
-			printf("built and set new, number %d\n", i);*/
-			
-			
-			/* This was only useful if we added metadata in front of all pages (I think)
-			memStruct * tempNext = memHead;
-			int i = 1;
-			while (i < pageCount){
-				printf("Need more pages, so searching for next free %d\n", i);
-				tempNext->next = findNext();				
-				i++;
-			}
-			*/
-			
-/**Annes Code**/
-/*if(type){
-		printf("Coming from library\n");
-		//why handle this differently?
-	}
-	else{
-		printf("Coming from macro\n");
-		printf("page size: %d\n", PAGESIZE);
-		//Asking for 0 bytes
-		if(size == 0){	//mallocing 0 bytes
-			printf("No bytes requested, file: %s, line: %d\n", file, line);
-			return NULL;
-		}
-		else{	//mallocing > 0 bytes
-			printf("%d bytes requested\n", size);
-		}
-		//first spot free.
-		if(memory[0] == 0){
-			printf("it worked\n");
-		}
-		else{
-			int i = 0;
-			while(memory[i] != 0){
-				i += PAGESIZE; //check page
-			}	
-			printf("nope\n");
-		}
-	}
-	return NULL;*/
