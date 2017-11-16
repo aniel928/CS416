67a6cbc3691997d1464f68bb4cc8790db5f2ba0c
Anne L Whitman <anne.whitman@rutgers.edu>
2017-11-16 23:05:49 +0000

    this is actually two test files.  Top part is what Anne has been using, bottom part (totally commented out) is what Mike has been using.

--- testfile.c
+++ testfile.c
@@ -16,52 +16,365 @@
 // 	pthread_exit((void*)&a);
 // }
 
-void giveMeMem(){
-	printf("mallocing 5000 bytes\n");
-	char* ptr[1000];
+
+/* * * * * * * * * * 5 MB 200 times each thread, 64 threads * * * * * * * * * *
+
+void giveMeMem(int* thread){
+	printf("Thread #: %d\n", *thread);
+	char* ptr[200];
+	printf("mallocing 5MB\n");
 	int i = 0;
-	while(i < 1000){
-		printf("request # %d\n", i);
-		ptr[i] = (char*)malloc(5000);
-		i++;
+	while(i < 200){
+		ptr[i] = (char*)malloc(5*1024*1024);
+		if(ptr[i]){
+			printf("malloced %d pointer: %p\n",i, ptr);
+			free(ptr[i]);
+			printf("pointer %d freed\n", i);
+			i++;
+		}
+		else{
+			printf("-------POINTER IS NULL-------\n");
+			return;
+		}
 	}
-	printf("malloced pointer: %p\n",ptr[0]);
-//	showData();
-	i = 0;
-	while(i < 1000){
+
+	pthread_exit(NULL);
+	return;
+}
+*/
+
+/* * * * * * * * * * 1 byte 1000 times each thread, free after allocating all 64 threads * * * * * * * * * 
+void giveMeMem(int* thread){
+	printf("Thread #: %d\n", *thread);
+	char* ptr[200];
+	printf("mallocing 5MB\n");
+	int i = 0;
+	while(i < 200){
+		ptr[i] = (char*)malloc(1);
+		if(ptr[i]){
+			printf("malloced %d pointer: %p\n",i, ptr[i]);
+//			free(ptr[i]);
+//			printf("pointer %d freed\n", i);
+			i++;
+		}
+		else{
+			printf("-------POINTER IS NULL-------\n");
+			return;
+		}
+	}
+	
+	i=0;
+	while(i < 200){
 		free(ptr[i]);
+		printf("pointer %d freed\n", i);
 		i++;
 	}
-//	printf("done freeing\n");
+
+	pthread_exit(NULL);
+	return;
+}
+*/
+
+/* * * * * * * * * * Shalloc * * * * * * * * * */
+void giveMeMem(int* thread){
+	printf("Thread #: %d\n", *thread);
+	char* ptr[200];
+	printf("mallocing 5MB\n");
+	int i = 0;
+	while(i < 200){
+		ptr[i] = (char*)malloc(5000);
+		if(ptr[i]){
+			printf("malloced %d pointer: %p\n",i, ptr[i]);
+//			free(ptr[i]);
+//			printf("pointer %d freed\n", i);
+			i++;
+		}
+		else{
+			printf("-------POINTER IS NULL-------\n");
+			return;
+		}
+	}
+	
+	i=0;
+	while(i < 200){
+		free(ptr[i]);
+		printf("pointer %d freed\n", i);
+		i++;
+	}
 
 	pthread_exit(NULL);
 	return;
 }
 
+void shallocIt(){
+	char* myptr = (char*)shalloc(5);
+	printf("myptr is %p\n", myptr);
+	myptr[0] = 'A';
+	myptr[1] = 'n';
+	myptr[2] = 'n';
+	myptr[3] = 'e';
+	myptr[4] = '\0';
+	
+	printf("name will be: %s\n", myptr);
+	pthread_exit(&myptr);
+	
+}
+
+void useIt(void** ptr){
+	printf("in Use it\n");
+ 	char* name = *ptr;
+ 	printf("My name is %s\n", name);
+}
 
 
 int main(int argc, char** argv){
-//	printf("Can i have 5 bytes?\n");
-	//void* myptr = malloc(5);//needs to be malloc call
-	//void* myptr2 = malloc(5);//needs to be malloc call
-//	printf("thanks!\n");
-	//free(myptr2);
-//	printf("not sure if it worked though\n");
-	//malloc(7);	
+
+/* * * * * Group tests * * * * */
 	pthread_t mythread[30];
-//	printf("Creating\n");
 	int i=0;
+	int j[30];
 	while(i< 30){
-		pthread_create(&mythread[i], NULL, (void*)&giveMeMem, NULL);
-		i++;		
+		j[i] = i;
+		pthread_create(&mythread[i], NULL, (void*)&giveMeMem, &j[i]);
+		printf("thread: %d\n", i);
+		i++;	
+			
 	}
-	//pthread_create(&mythread, NULL, (void*)&giveMeMem, NULL);
-//	printf("Joining\n");
 	i=0;
 	while(i< 30){
 		pthread_join(mythread[i], NULL);
 		i++;		
 	}
-	
+
+/* * * * * Small one of tests * * * * */
+// 	pthread_t mythread1, mythread2, mythread3;
+// 	char* ptr1 = NULL;
+// 	pthread_create(&mythread1, NULL, (void*)&shallocIt, NULL);
+// 	pthread_join(mythread1, &ptr1);
+// 
+// 	printf("testing name: %s\n", ptr1);
+// 	printf("testig pointer: %p\n", &ptr1);
+// 
+// 	pthread_create(&mythread2, NULL, (void*)&useIt, &ptr1);
+// 	pthread_create(&mythread3, NULL, (void*)&useIt, &ptr1);	
+// 	pthread_join(mythread2, NULL);
+// 	pthread_join(mythread3, NULL);			
+
+	printf("Test done\n");
 //	showData();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 
+// #include "my_pthread_t.h"
+// #include <time.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// //#include <pthread.h>
+// // #define NUM 30
+// // pthread_mutex_t* mutex1 = NULL;
+// 
+//  
+// // 
+// // void returnNumber(){
+// // 	int a = 4;
+// // 	printf("YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY\n");
+// // 	char* ptr = (char*)malloc(5000);
+// // 	free(ptr);
+// // 	pthread_exit((void*)&a);
+// // }
+// 
+// 
+// /* * * * * * * * * * 5 MB 200 times each thread, 64 threads * * * * * * * * * *
+// 
+// void giveMeMem(int* thread){
+// 	printf("Thread #: %d\n", *thread);
+// 	char* ptr[200];
+// 	printf("mallocing 5MB\n");
+// 	int i = 0;
+// 	while(i < 200){
+// 		ptr[i] = (char*)malloc(5*1024*1024);
+// 		if(ptr[i]){
+// 			printf("malloced %d pointer: %p\n",i, ptr);
+// 			free(ptr[i]);
+// 			printf("pointer %d freed\n", i);
+// 			i++;
+// 		}
+// 		else{
+// 			printf("-------POINTER IS NULL-------\n");
+// 			return;
+// 		}
+// 	}
+// 
+// 	pthread_exit(NULL);
+// 	return;
+// }
+// */
+// 
+// /* * * * * * * * * * 1 byte 1000 times each thread, free after allocating all 64 threads * * * * * * * * * 
+// void giveMeMem(int* thread){
+// 	printf("Thread #: %d\n", *thread);
+// 	char* ptr[200];
+// 	printf("mallocing 5MB\n");
+// 	int i = 0;
+// 	while(i < 200){
+// 		ptr[i] = (char*)malloc(1);
+// 		if(ptr[i]){
+// 			printf("malloced %d pointer: %p\n",i, ptr[i]);
+// //			free(ptr[i]);
+// //			printf("pointer %d freed\n", i);
+// 			i++;
+// 		}
+// 		else{
+// 			printf("-------POINTER IS NULL-------\n");
+// 			return;
+// 		}
+// 	}
+// 	
+// 	i=0;
+// 	while(i < 200){
+// 		free(ptr[i]);
+// 		printf("pointer %d freed\n", i);
+// 		i++;
+// 	}
+// 
+// 	pthread_exit(NULL);
+// 	return;
+// }
+// */
+// 
+// /* * * * * * * * * * Shalloc * * * * * * * * * */
+// void giveMeMem(int* thread){
+// 	printf("Thread #: %d\n", *thread);
+// 	char* ptr[2];
+// 	printf("mallocing 16000\n");
+// 	int i = 0;
+// 	while(i < 2){
+// 		ptr[i] = (char*)malloc(16000);
+// 		if(ptr[i]){
+// //			printf("malloced %d pointer: %p\n",i, ptr[i]);
+// //			free(ptr[i]);
+// //			printf("pointer %d freed\n", i);
+// 			i++;
+// 		}
+// 		else{
+// 			printf("-------POINTER IS NULL-------\n");
+// 			return;
+// 		}
+// 	}
+// 	i = 0;
+// 	while(i < 2){
+// 		if(ptr[i]){
+// //			printf("malloced %d pointer: %p\n",i, ptr[i]);
+// 			free(ptr[i]);
+// //			printf("pointer %d freed\n", i);
+// 		
+// 		}
+// 			i++;
+// 		
+// 	}
+// 	pthread_exit(NULL);
+// 
+// 	
+// 	
+// 	return;
+// }
+// 	
+// 	//i=0;
+// 	/*while(i < 3){
+// 		free(ptr[i]);
+// 		printf("pointer %d freed\n", i);
+// 		i++;
+// 	}*/
+// //}
+// 
+// /*
+// void* shallocIt(){
+// 	char* myptr = NULL;
+// 	myptr = (char*)shalloc(5);
+// 	myptr[0] = 'A';
+// 	myptr[1] = 'n';
+// 	myptr[2] = 'n';
+// 	myptr[3] = 'e';
+// 	myptr[4] = '\0';
+// 	
+// 	pthread_exit(&myptr);
+// }
+// */
+// void useIt(void** ptr){
+// 		char * thing6 = (char*)myallocate(5000, __FILE__, __LINE__, 1);
+// 		
+// 		char * thing7 = (char*)myallocate(5000, __FILE__, __LINE__, 1);
+// 		
+// 		char * thing8 = (char*)myallocate(5000, __FILE__, __LINE__, 1);
+// 		
+// 		char * thing9 = (char*)myallocate(5000, __FILE__, __LINE__, 1);
+// }
+// 
+// 
+// int main(int argc, char** argv){
+// 
+// /* * * * * Group tests * * * * */
+// 	pthread_t mythread[5];
+//  	int i=0;
+//  	int j[5];
+//  	while(i< 5){
+//  		j[i] = i;
+// 		pthread_create(&mythread[i], NULL, (void*)&giveMeMem, &j[i]);
+//  		printf("thread: %d\n", i);
+//  		i++;		
+//  	}
+//  	i=0;
+//  	while(i< 5){
+//  		pthread_join(mythread[i], NULL);
+//  		i++;		
+//  	}
+// 
+// /* * * * * Small one of tests * * * * */
+// 	pthread_t mythread1, mythread2, mythread3;
+// 	char* ptr1 = NULL;
+// 	//pthread_create(&mythread1, NULL, (void*)&shallocIt, NULL);
+// 	//pthread_create(&mythread2, NULL, (void*)&useIt, &ptr1);
+// 	//	pthread_create(&mythread3, NULL, (void*)&useIt, &ptr1);
+// 	//	pthread_create(&mythread2, NULL, (void*)&useIt, &ptr1);
+// 
+// 		
+// 	
+// /*
+// 		char * thing1 = (char*)malloc(5000);
+// 		
+// 		char * thing2 = (char*)malloc(5000);
+// 		
+// 		char * thing3 = (char*)malloc(5000);
+// 		
+// 		char * thing4 = (char*)malloc(5000);
+// 		
+// 		char * thing5 = (char*)malloc(5000);
+// 		
+// */
+// 		
+// 		//free (name);
+// 
+// 	
+// 	
+// //	pthread_join(mythread1, (void*)ptr1);
+// //	pthread_join(mythread2, NULL);
+// 	//pthread_join(mythread3, NULL);			
+// 
+// 	printf("Test done\n");
+// 	dataEPT();
+// 	printf("----\n");
+// //	showData();
+// }
